eafcbb2babdedf6f6bf5829937258e558aeb5349
Naresh Krishna <nareshkrishna620@gmail.com>
2025-11-20 21:00:46 +0530

    Update top deals

---
+++ topdeals.js
@@ -0,0 +1,60 @@
+const topdealslist = [
+  {
+    title: "Projector",
+    price: "From ₹ 6990",
+    image:
+      "https://rukminim1.flixcart.com/image/240/240/xif0q/projector/h/e/n/atom-2x-10-5-ei9027-led-projector-egate-original-imahhsqm5ctgzae3.jpeg?q=60",
+  },
+  {
+    title: "Best Selling mobile Speaker",
+    price: "From ₹ 499",
+    image:
+      "https://rukminim1.flixcart.com/image/240/240/kcf4lu80/speaker/mobile-tablet-speaker/h/u/f/srs-xb23-sony-original-imaftk66vjxp86h5.jpeg?q=60",
+  },
+  {
+    title: "Monitors",
+    price: "From ₹ 6599",
+    image:
+      "https://rukminim1.flixcart.com/image/240/240/xif0q/monitor/i/q/k/-original-imahbzhcdvc6gkhu.jpeg?q=60",
+  },
+  {
+    title: "Monitor",
+    price: "From ₹ 8279",
+    image:
+      "https://rukminim1.flixcart.com/image/240/240/xif0q/monitor/w/i/f/-original-imahagy2cefszqgy.jpeg?q=60",
+  },
+  {
+    title: "Top Mirroeless Camera",
+    price: "Shop Now!",
+    image:
+      "https://rukminim1.flixcart.com/image/240/240/xif0q/camera/s/o/y/na-digital-kids-camera-20mp-1080p-32gb-card-supported-mini-original-imagy2znwq4uy4z8.jpeg?q=60",
+  },
+  {
+    title: "Fastrack SmartWatch",
+    price: "From ₹ 1,399",
+    image:
+      "https://rukminim1.flixcart.com/image/240/240/xif0q/smartwatch/5/v/s/-original-imagxrhetgfuebnn.jpeg?q=60",
+  },
+  {
+    title: "Best true Wireless HeadPhone",
+    price: "Grab Now",
+    image:
+      "https://rukminim1.flixcart.com/image/240/240/l58iaa80/headphone/k/z/m/nord-buds-ce-oneplus-original-imagfyk4hyvgg6ze.jpeg?q=60",
+  },
+  {
+    title: "Printer",
+    price: "From ₹ 2339",
+    image:
+      "https://rukminim1.flixcart.com/image/240/240/xif0q/printer/s/8/d/-original-imafkykednshkhx5.jpeg?q=60",
+  },
+];
+
+const topdeals = document.getElementById("topdeals");
+
+topdealslist.map((list) => {
+  topdeals.innerHTML += `<div class="topdeals-card">
+    <img src="${list.image}" alt="${list.title}" />
+    <h3>${list.title}</h3>
+    <p>${list.price}</p>
+  </div>`;
+});
